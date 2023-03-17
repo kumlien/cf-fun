cfdde7e1d91426cd8f7eb18647b7825af08b1ff1
Svante Kumlien <svante.kumlien@gmail.com>
2023-03-17 08:47:53 +0100

    wip

---
+++ src/app.d.ts
@@ -0,0 +1,22 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		interface Platform {
+			       env: {
+			           COUNTER: DurableObjectNamespace;
+			       };
+			       context: {
+		           waitUntil(promise: Promise<any>): void;
+			       };
+			       caches: CacheStorage & { default: Cache }
+			   }
+	}
+}
+
+export async function post({ request, platform }) {
+	const counter = platform.env.COUNTER.idFromName("A");
+  }
